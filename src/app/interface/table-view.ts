a579e3b7df7bb156cecda4105b4bb0dc98a07a40
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2023-07-28 19:11:58 +0700

    feat: aspect extraction frontend

---
+++ src/app/interface/table-view.ts
@@ -0,0 +1,10 @@
+export interface TableView {
+  no: number;
+  kalimat: string;
+  aspek: string;
+  opini: string;
+  trueTuple: string;
+  sentiment: string;
+  metaAspect: string;
+  metaOpinion: string;
+}
--- src/app/pages/aspect-extraction-page/aspect-extraction-page.component.ts
+++ src/app/pages/aspect-extraction-page/aspect-extraction-page.component.ts
@@ -1,10 +1,84 @@
 import { Component } from '@angular/core';
+import { TableView } from 'src/app/interface/table-view';
 
 @Component({
   selector: 'app-aspect-extraction-page',
   templateUrl: './aspect-extraction-page.component.html',
-  styleUrls: ['./aspect-extraction-page.component.css']
+  styleUrls: ['./aspect-extraction-page.component.css'],
 })
 export class AspectExtractionPageComponent {
+  datas: TableView[];
+  isValid: boolean = false;
+  isInvalid: boolean = false;
 
+  constructor() {
+    this.datas = [
+      {
+        no: 1,
+        kalimat: 'Tweet Saya ini loh hahahahaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        aspek: 'Perubahan',
+        opini: 'jelek',
+        trueTuple: '',
+        sentiment: '',
+        metaAspect: '{ lemma }',
+        metaOpinion: '{ lemma }',
+      },
+      {
+        no: 2,
+        kalimat:
+          'Tweet Saya ini loh hahahahaiiiiiiiiiiiiiiiiiiiiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        aspek: 'Perubahan',
+        opini: 'jelek',
+        trueTuple: '',
+        sentiment: '',
+        metaAspect:
+          '{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }',
+        metaOpinion:
+          '{ lemma } { lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }',
+      },
+      {
+        no: 3,
+        kalimat: 'Tweet Saya ini loh hahahahaiiiiiiiiiiiiiiiiiiiiiii',
+        aspek: 'Perubahan',
+        opini: 'jelek',
+        trueTuple: '',
+        sentiment: '',
+        metaAspect: '{ lemma }',
+        metaOpinion: '{ lemma }',
+      },
+      {
+        no: 1,
+        kalimat: 'Tweet Saya ini loh hahahahaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        aspek: 'Perubahan',
+        opini: 'jelek',
+        trueTuple: '',
+        sentiment: '',
+        metaAspect: '{ lemma }',
+        metaOpinion: '{ lemma }',
+      },
+      {
+        no: 2,
+        kalimat:
+          'Tweet Saya ini loh hahahahaiiiiiiiiiiiiiiiiiiiiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        aspek: 'Perubahan',
+        opini: 'jelek',
+        trueTuple: '',
+        sentiment: '',
+        metaAspect:
+          '{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }',
+        metaOpinion:
+          '{ lemma } { lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }{ lemma }',
+      },
+      {
+        no: 3,
+        kalimat: 'Tweet Saya ini loh hahahahaiiiiiiiiiiiiiiiiiiiiiii',
+        aspek: 'Perubahan',
+        opini: 'jelek',
+        trueTuple: '',
+        sentiment: '',
+        metaAspect: '{ lemma }',
+        metaOpinion: '{ lemma }',
+      },
+    ];
+  }
 }
